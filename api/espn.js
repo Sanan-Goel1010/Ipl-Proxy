90edc15b682664fbb27a4cc9fb0e0060c6f19414
Sanan-Goel1010 <sanan.goel@sourcefuse.com>
2026-04-24 22:43:48 -0400

    Update espn.js

--- api/espn.js
+++ api/espn.js
@@ -1,15 +1,17 @@
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  const { matchId, type } = req.query;
+  const { matchId, type, seriesId } = req.query;
 
   try {
     let url;
     if (type === 'recent') {
       url = 'https://cricbuzz-cricket.p.rapidapi.com/matches/v1/recent';
+    } else if (type === 'series' && seriesId) {
+      url = `https://cricbuzz-cricket.p.rapidapi.com/series/v1/${seriesId}/matches`;
     } else if (matchId) {
       url = `https://cricbuzz-cricket.p.rapidapi.com/mcenter/v1/${matchId}/scard`;
     } else {
-      return res.status(400).json({ error: 'Provide matchId or type=recent' });
+      return res.status(400).json({ error: 'Provide matchId, type=recent, or type=series&seriesId=X' });
     }
 
     const response = await fetch(url, {
